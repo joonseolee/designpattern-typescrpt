f3f7fea48959df418b6e323e5b4d933573f4746a
joonseolee <niceweather94@gmail.com>
2022-02-20 14:55:09 +0900

    fix(): 추상팩토리패턴 오류 수정

--- __tests__/abstract-factory/main.test.ts
+++ __tests__/abstract-factory/main.test.ts
@@ -4,17 +4,21 @@ import { SamsungKeyboard } from 'src/abstract-factory/samsung-keyboard';
 import { ProduceComputerFactory } from 'src/abstract-factory/produce-computer-factory';
 import { LGMouse } from 'src/abstract-factory/lg-mouse';
 import { LGKeyboard } from 'src/abstract-factory/lg-keyboard';
+import { SamsungComputerFactory } from 'src/abstract-factory/samsung-computer-factory';
+import { LGComputerFactory } from 'src/abstract-factory/lg-computer-factory';
 
 describe('abstract-factory pattern tests', () => {
   it('generate samsung computer', () => {
     const samsungComputer: Computer = ProduceComputerFactory.createComputer(
-      'SAMSUNG',
+      new SamsungComputerFactory(),
     );
     expect(samsungComputer.mouse).toBeInstanceOf(SamsungMouse);
     expect(samsungComputer.keyboard).toBeInstanceOf(SamsungKeyboard);
   });
   it('generate lg computer', () => {
-    const lgComputer: Computer = ProduceComputerFactory.createComputer('LG');
+    const lgComputer: Computer = ProduceComputerFactory.createComputer(
+      new LGComputerFactory(),
+    );
     expect(lgComputer.mouse).toBeInstanceOf(LGMouse);
     expect(lgComputer.keyboard).toBeInstanceOf(LGKeyboard);
   });
--- src/abstract-factory/produce-computer-factory.ts
+++ src/abstract-factory/produce-computer-factory.ts
@@ -1,21 +1,6 @@
 import { ComputerFactory } from './computer-factory';
-import { FactoryType } from './factory-type';
-import { LGComputerFactory } from './lg-computer-factory';
-import { SamsungComputerFactory } from './samsung-computer-factory';
-
 export class ProduceComputerFactory {
-  static createComputer(type: FactoryType) {
-    let computerFactory: ComputerFactory;
-    switch (type) {
-      case 'LG':
-        computerFactory = new LGComputerFactory();
-        break;
-      case 'SAMSUNG':
-        computerFactory = new SamsungComputerFactory();
-        break;
-      default:
-        throw new Error('Not matched computer company');
-    }
+  static createComputer(computerFactory: ComputerFactory) {
     return computerFactory.createComputer();
   }
 }
